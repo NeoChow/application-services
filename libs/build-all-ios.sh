bc4de0188538d4ffdac279d18869bfb53e903376
Edouard Oger <eoger@fastmail.com>
2018-12-13 16:31:10 -0500

    fix scripts

--- libs/build-all-ios.sh
+++ libs/build-all-ios.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-set -e
+set -euvx
 
 IOS_MIN_SDK_VERSION="9.0"
+# Our short-names for the architectures.
 TARGET_ARCHS=("i386" "x86_64" "armv7" "arm64")
-TARGET_ARCHS_HOSTS=("i386-apple-darwin" "x86_64-apple-darwin" "armv7-apple-darwin" "arm-apple-darwin")
-
-# End of configuration.
 
 if [ "$#" -ne 2 ]
 then
@@ -34,28 +32,37 @@ function universal_lib() {
   fi
 }
 
-if [ -d "ios" ]; then
-  echo "ios folder already exists. Skipping build."
-  exit 0
-fi
-
 echo "# Building openssl"
 for i in "${!TARGET_ARCHS[@]}"; do
   ARCH=${TARGET_ARCHS[$i]}
   DIST_DIR=$(abspath "ios/""$ARCH""/openssl")
-  ./build-openssl-ios.sh "$OPENSSL_SRC_PATH" "$DIST_DIR" "$ARCH" "$IOS_MIN_SDK_VERSION"
-  DIST_DIR=$(abspath "ios/""$ARCH""/sqlcipher")
-  HOST=${TARGET_ARCHS_HOSTS[$i]}
-  ./build-sqlcipher-ios.sh "$SQLCIPHER_SRC_PATH" "$DIST_DIR" "$ARCH" "$HOST" "$IOS_MIN_SDK_VERSION"
+  if [ -d "$DIST_DIR" ]; then
+    echo "$DIST_DIR already exists. Skipping building openssl."
+  else
+    ./build-openssl-ios.sh "$OPENSSL_SRC_PATH" "$DIST_DIR" "$ARCH" "$IOS_MIN_SDK_VERSION" || exit 1
+  fi
 done
 universal_lib "openssl" "libssl.a"
 universal_lib "openssl" "libcrypto.a"
+
+echo "# Building sqlcipher"
+for i in "${!TARGET_ARCHS[@]}"; do
+  ARCH=${TARGET_ARCHS[$i]}
+  DIST_DIR=$(abspath "ios/""$ARCH""/sqlcipher")
+  if [ -d "$DIST_DIR" ]; then
+    echo "$DIST_DIR already exists. Skipping building sqlcipher."
+  else
+    ./build-sqlcipher-ios.sh "$SQLCIPHER_SRC_PATH" "$DIST_DIR" "$ARCH" "$IOS_MIN_SDK_VERSION" || exit 1
+  fi
+done
 universal_lib "sqlcipher" "libsqlcipher.a"
 
 HEADER_DIST_DIR="ios/universal/openssl/include/openssl"
 if [ ! -e "$HEADER_DIST_DIR" ]; then
   mkdir -p $HEADER_DIST_DIR
   cp -L "$OPENSSL_SRC_PATH"/include/openssl/*.h "$HEADER_DIST_DIR"
+  # The following file is generated during compilation, we pick the one in arm64.
+  cp -L "$PWD"/ios/arm64/openssl/include/openssl/opensslconf.h "$HEADER_DIST_DIR"
 fi
 
 HEADER_DIST_DIR="ios/universal/sqlcipher/include/sqlcipher"
--- libs/build-sqlcipher-ios.sh
+++ libs/build-sqlcipher-ios.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
+# This script downloads and builds the SQLcipher library for iOS.
+
 set -euvx
 
-if [ "$#" -ne 5 ]
+if [ "$#" -ne 4 ]
 then
     echo "Usage:"
-    echo "./build-sqlcipher-ios.sh <ABSOLUTE_SRC_DIR> <DIST_DIR> <ARCH> <HOST> <IOS_MIN_SDK_VERSION>"
+    echo "./build-sqlcipher-ios.sh <ABSOLUTE_SRC_DIR> <DIST_DIR> <ARCH> <IOS_MIN_SDK_VERSION>"
     exit 1
 fi
 
 SQLCIPHER_SRC_DIR=$1
 DIST_DIR=$2
 ARCH=$3
-HOST=$4
-IOS_MIN_SDK_VERSION=$5
+IOS_MIN_SDK_VERSION=$4
 
 if [ -d "$DIST_DIR" ]; then
   echo "$DIST_DIR"" folder already exists. Skipping build."
@@ -25,32 +26,51 @@ mkdir -p "$SQLCIPHER_IOS"
 pushd "$SQLCIPHER_IOS"
 
 if [[ "${ARCH}" == "i386" || "${ARCH}" == "x86_64" ]]; then
-  PLATFORM="iPhoneSimulator"
+  OS_COMPILER="iPhoneSimulator"
+  if [[ "${ARCH}" == "x86_64" ]]; then
+    HOST="x86_64-apple-darwin"
+  else
+    HOST="x86-apple-darwin"
+  fi
+elif [[ "${ARCH}" == "armv7" || "${ARCH}" == "arm64" ]]; then
+  OS_COMPILER="iPhoneOS"
+  HOST="arm-apple-darwin"
 else
-  PLATFORM="iPhoneOS"
+  echo "Unsupported architecture"
+  exit 1
 fi
 
 DEVELOPER=$(xcode-select -print-path)
-export PLATFORM="$PLATFORM"
-export CROSS_TOP="${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer"
-export CROSS_SDK="${PLATFORM}.sdk"
-export BUILD_TOOLS="${DEVELOPER}"
-export CC="$BUILD_TOOLS/usr/bin/gcc"
-export LD="$BUILD_TOOLS/usr/bin/ld"
-#
+export CROSS_TOP="${DEVELOPER}/Platforms/${OS_COMPILER}.platform/Developer"
+export CROSS_SDK="${OS_COMPILER}.sdk"
+TOOLCHAIN_BIN="${DEVELOPER}/Toolchains/XcodeDefault.xctoolchain/usr/bin"
+export CC="${TOOLCHAIN_BIN}/clang"
+export AR="${TOOLCHAIN_BIN}/ar"
+export RANLIB="${TOOLCHAIN_BIN}/ranlib"
+export STRIP="${TOOLCHAIN_BIN}/strip"
+export LIBTOOL="${TOOLCHAIN_BIN}/libtool"
+export NM="${TOOLCHAIN_BIN}/nm"
+export LD="${TOOLCHAIN_BIN}/ld"
+
 CFLAGS="\
   -fembed-bitcode \
   -arch ${ARCH} \
   -isysroot ${CROSS_TOP}/SDKs/${CROSS_SDK} \
-  -miphoneos-version-min=${IOS_MIN_SDK_VERSION} \
+  -mios-version-min=${IOS_MIN_SDK_VERSION} \
 "
 
-if [[ "$ARCH" == "armv7" ]]; then
-  CFLAGS="$CFLAGS -mno-thumb"
-fi
-
-SQLCIPHER_DEFINES=" \
+# Keep in sync with SQLCIPHER_CFLAGS in `build-sqlcipher-desktop.sh` for now (it probably makes
+# sense to try to avoid this duplication in the future).
+# TODO: We could probably prune some of these, and it would be nice to allow debug builds (which
+# should set `SQLITE_DEBUG` and `SQLITE_ENABLE_API_ARMOR` and not `NDEBUG`).
+SQLCIPHER_CFLAGS=" \
   -DSQLITE_HAS_CODEC \
+  -DSQLITE_SOUNDEX \
+  -DHAVE_USLEEP=1 \
+  -DSQLITE_MAX_VARIABLE_NUMBER=99999 \
+  -DSQLITE_THREADSAFE=1 \
+  -DSQLITE_DEFAULT_JOURNAL_SIZE_LIMIT=1048576 \
+  -DNDEBUG=1 \
   -DSQLITE_ENABLE_MEMORY_MANAGEMENT=1 \
   -DSQLITE_ENABLE_LOAD_EXTENSION \
   -DSQLITE_ENABLE_COLUMN_METADATA \
@@ -62,17 +82,22 @@ SQLCIPHER_DEFINES=" \
   -DSQLITE_ENABLE_FTS3_PARENTHESIS \
   -DSQLITE_ENABLE_FTS4 \
   -DSQLITE_ENABLE_FTS5 \
-  -DSQLITE_ENABLE_DBSTAT_VTA \
+  -DSQLITE_ENABLE_DBSTAT_VTAB \
+  -DSQLITE_SECURE_DELETE \
+  -DSQLITE_DEFAULT_PAGE_SIZE=32768 \
+  -DSQLITE_MAX_DEFAULT_PAGE_SIZE=32768 \
 "
 
 ../configure \
+  --with-pic \
+  --disable-tcl \
   --host="$HOST" \
   --verbose \
   --with-crypto-lib=commoncrypto \
   --enable-tempstore=yes \
   --enable-threadsafe=yes \
   --disable-editline \
-  CFLAGS="$CFLAGS $SQLCIPHER_DEFINES" \
+  CFLAGS="${CFLAGS} ${SQLCIPHER_CFLAGS}" \
   LDFLAGS="-framework Security -framework Foundation"
 
 # Make all fails because it tries to build the command line program.
